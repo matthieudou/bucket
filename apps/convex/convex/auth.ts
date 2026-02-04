6554f4813f8ba598cafacf41d498f6aee90f3733
Matthieu d'Oultremont <matthieu.doultremont@gmail.com>
2026-02-04 10:24:07 +0100

    Align password provider import

--- apps/convex/convex/auth.ts
+++ apps/convex/convex/auth.ts
@@ -1,6 +1,6 @@
 import { convexAuth } from "@convex-dev/auth/server";
 import Google from "@auth/core/providers/google";
-import Password from "@convex-dev/auth/providers/Password";
+import { Password } from "@convex-dev/auth/providers/Password";
 import { Anonymous } from "@convex-dev/auth/providers/Anonymous";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
